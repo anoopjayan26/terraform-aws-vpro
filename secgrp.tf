c37714e6959c6d482613d2fbf84bad43e3abb9bd
anoopjayan26 <anoopjayan26@gmail.com>
2024-03-11 13:14:51 -0400

    "Commit change in secgrp"

--- secgrp.tf
+++ secgrp.tf
@@ -2,7 +2,7 @@ resource "aws_security_group" "vprofile-bean-elb-sg" {
   name        = "vprofile-bean-elb-sg"
   description = "Security group from bean-elb"
   vpc_id      = module.vpc.vpc_id
-  egress = {
+  egress {
     from_port   = 0
     protocol    = "-1"
     to_port     = 0
@@ -21,14 +21,14 @@ resource "aws_security_group" "vprofile-bastion-sg" {
   name        = "vprofile-bastion-sg"
   description = "Security group for bastion EC2 instance"
   vpc_id      = module.vpc.vpc_id
-  egress = {
+  egress {
     from_port   = 0
     protocol    = "-1"
     to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress = {
+  ingress {
     from_port   = 22
     protocol    = "tcp"
     to_port     = 22
