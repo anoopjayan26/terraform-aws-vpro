c01abe9ce68d805ed84977ebc834888b553235a0
anoopjayan26 <anoopjayan26@gmail.com>
2024-03-11 13:03:30 -0400

    "Commit change in secgrp"

--- secgrp.tf
+++ secgrp.tf
@@ -35,3 +35,54 @@ resource "aws_security_group" "vprofile-bastion-sg" {
         cidr_blocks = [var.MYIP]
     }
 }
+
+resource "aws_security_group" "vprofile-prod-sg" {
+    name        = "vprofile-prod-sg"
+    description = "Security group for beanstalk instances"
+    vpc_id      = module.vpc.vpc_id
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port       = 22
+        protocol        = "tcp"
+        to_port         = 22
+        security_groups = [aws_security_group.vprofile-bastion-sg.id]
+    }
+}
+
+resource "aws_security_group" "vprofile-backend-sg" {
+    name        = "vprofile-backend-sg"
+    description = "Security group for RDS, active mq, elastic cache"
+    vpc_id      = module.vpc.vpc_id
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port       = 0
+        protocol        = "-1"
+        to_port         = 0
+        security_groups = [aws_security_group.vprofile-prod-sg.id]
+    }
+    ingress {
+        from_port       = 3306
+        protocol        = "tcp"
+        to_port         = 3306
+        security_groups = [aws_security_group.vprofile-bastion-sg.id]
+    }
+}
+
+resource "aws_security_group_rule" "sec_group_allow_itself" {
+    type                     = "ingress"
+    from_port                = 0
+    to_port                  = 65535
+    protocol                 = "tcp"
+    security_group_id        = aws_security_group.vprofile-backend-sg.id
+    source_security_group_id = aws_security_group.vprofile-backend-sg.id
+}
